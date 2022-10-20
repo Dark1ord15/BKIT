c6ff0326d5a9a7293d68cc060bc47c55d922d399
Dark1ord15 <116286629+Dark1ord15@users.noreply.github.com>
2022-10-20 17:08:33 +0300

    Add files via upload

---
+++ Lab2/main.py
@@ -0,0 +1,15 @@
+from lab_python_oop.rectangle import Rectangle
+from lab_python_oop.circle import Circle
+from lab_python_oop.square import Square
+
+
+def main():
+    r = Rectangle("синего", 7, 6)
+    c = Circle("зеленого", 7)
+    s = Square("красного", 7)
+    print(r)
+    print(c)
+    print(s)
+
+if __name__ == "__main__":
+    main()
